3be0cc2c2b85cafdb7876ef81a7440f03516b31e
FusionTech <fusion@fusion.com>
2021-04-01 15:41:00 +0300

    feature: configure cors

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const app = require('./server');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.use(routes);
 
