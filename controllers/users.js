759c9b890f106f41b4b1c1c717bf1eb5dc6ed0cd
FusionTech <fusion@fusion.com>
2021-04-06 14:12:30 +0300

    feature: create users controllers

---
+++ controllers/users.js
@@ -0,0 +1,68 @@
+const models = require('../db/models');
+const NotFoundError = require('../errors/NotFoundError');
+
+const userNotFoundErr = new NotFoundError('There is no user with this id.');
+
+module.exports.getUsers = async (req, res, next) => {
+  try {
+    const allUsers = await models.User.findAll({
+      raw: true,
+    });
+
+    res.send(allUsers);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const user = await models.User.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!user) {
+      throw userNotFoundErr;
+    }
+
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.removeUser = async (req, res, next) => {
+  try {
+    const user = await models.User.findByPk(req.params.id);
+
+    if (!user) {
+      throw userNotFoundErr;
+    }
+
+    await models.User.destroy({ where: { id: req.params.id } });
+
+    res.status(200).json({ message: 'The user was successfully deleted.' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateUserInfo = async (req, res, next) => {
+  try {
+    const { name, email, dob } = req.body;
+
+    const user = await models.User.update({ name, email, dob }, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!user) {
+      throw userNotFoundErr;
+    }
+
+    res.status(200).json({ message: 'The user was successfully updated.' });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
-// const { validateUserInfo } = require('../middlewares/reqValidation');
-// const checkIsForbiddenRout = require('../middlewares/isForbiddenRout');
-// const controllers = require('../controllers/users');
+const { validateUserInfo } = require('../middlewares/reqValidation');
+const checkIsForbiddenRout = require('../middlewares/isForbiddenRout');
+const controllers = require('../controllers/users');
 
-// router.get('/', controllers.getUsers);
-// router.get('/:id', checkIsForbiddenRout, controllers.getUser);
-// router.patch('/:id', checkIsForbiddenRout, validateUserInfo, controllers.updateUserInfo);
-// router.delete('/:id', checkIsForbiddenRout, controllers.removeUser);
+router.get('/', controllers.getUsers);
+router.get('/:id', checkIsForbiddenRout, controllers.getUser);
+router.patch('/:id', checkIsForbiddenRout, validateUserInfo, controllers.updateUserInfo);
+router.delete('/:id', checkIsForbiddenRout, controllers.removeUser);
 
-// module.exports = router;
+module.exports = router;
