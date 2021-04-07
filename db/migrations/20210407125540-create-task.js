f94d5bed2dcbbcd768e4f63e9c9cab76b8a3ed19
FusionTech <fusion@fusion.com>
2021-04-07 15:58:45 +0300

    feature: create task model

---
+++ db/migrations/20210407125540-create-task.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable lines-around-directive */
+/* eslint-disable strict */
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        validate: {
+          len: [1, 20],
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  },
+};
---
+++ db/models/task.js
@@ -0,0 +1,35 @@
+/* eslint-disable strict */
+/* eslint-disable lines-around-directive */
+/* eslint-disable no-unused-vars */
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Task.init(
+    {
+      text: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          len: [1, 20],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Task',
+    },
+  );
+  return Task;
+};
