5b07ef40e76d601816d5b08e65b86e9be96afb92
FusionTech <fusion@fusion.com>
2021-04-09 09:46:47 +0300

    feature: add endpoint routes for boards, columns & tasks

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ const router = require('express').Router();
 
 const authRouter = require('./auth');
 const usersRouter = require('./users');
+const boardsRouter = require('./boards');
+const columnsRouter = require('./columns');
+const tasksRouter = require('./tasks');
 const auth = require('../middlewares/auth');
 
 router.use('/', authRouter);
 router.use(auth);
 router.use('/users', usersRouter);
+router.use('/boards', boardsRouter);
+router.use('/columns', columnsRouter);
+router.use('/tasks', tasksRouter);
 
 router.use('*', (req, res) => res.status(404)
   .json({
