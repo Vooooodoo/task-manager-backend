e52266e1bc56a68e7f0aec369a06c52fbba7061f
FusionTech <fusion@fusion.com>
2021-04-22 10:57:25 +0300

    refactor: improve KISS

--- middlewares/reqValidation.js
+++ middlewares/reqValidation.js
@@ -2,31 +2,76 @@ const { celebrate, Joi } = require('celebrate');
 
 const validateNewUser = celebrate({
   body: Joi.object().keys({
-    firstName: Joi.string().required().min(2).max(40),
-    lastName: Joi.string().required().min(2).max(40),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    firstName: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .pattern(/^[a-zA-Zs-]+$/),
+    lastName: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .pattern(/^[a-zA-Zs-]+$/),
+    email: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .email(),
+    password: Joi
+      .string()
+      .required()
+      .min(8)
+      .max(40),
   }),
 });
 
 const validateLogin = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    email: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .email(),
+    password: Joi
+      .string()
+      .required()
+      .min(8)
+      .max(40),
   }),
 });
 
 const validateUserInfo = celebrate({
   body: Joi.object().keys({
-    firstName: Joi.string().required().min(2).max(40),
-    lastName: Joi.string().required().min(2).max(40),
-    about: Joi.string().allow('').min(2).max(400),
+    firstName: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .pattern(/^[a-zA-Zs-]+$/),
+    lastName: Joi
+      .string()
+      .required()
+      .min(2)
+      .max(40)
+      .pattern(/^[a-zA-Zs-]+$/),
+    about: Joi
+      .string()
+      .allow('')
+      .min(2)
+      .max(400),
   }),
 });
 
 const validateUserRoleId = celebrate({
   body: Joi.object().keys({
-    roleId: Joi.string().required().pattern(/^(admin|user)$/),
+    roleId: Joi
+      .string()
+      .required()
+      .pattern(/^(admin|user)$/),
   }),
 });
 
